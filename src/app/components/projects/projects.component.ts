a87751aaf32cae060c20b9defeafd13c033abb10
karl doherty <karldoherty15@icloud.com>
2022-11-12 23:30:09 +0000

    Working on About page, route animations added

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'; 
+import { faChevronLeft, faChevronRight, faL } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -7,8 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+  projectIndex: number = 0;
+  leftArrowDisabled!: boolean;
+  rightArrowDisabled!: boolean;
+
+  projectsList : any = [
+    {
+      title: "PremNews",
+      imgSrc: "exampleSrc",
+      description: "ExampleDescription"
+    },
+    {
+      title: "Project 2",
+      imgSrc: "exampleSrc2",
+      description: "ExampleDescription2"
+    }
+  ]
   constructor() { }
   ngOnInit(): void {
+    this.leftArrowDisabled = true;
+    this.rightArrowDisabled = false;
   }
 
+  changeProject(direction: number) {
+    this.projectIndex = this.projectIndex+=direction;
+    this.isArrowEnabled();
+  }
+
+  isArrowEnabled() {
+    if (this.projectIndex == 0) {
+      this.leftArrowDisabled = true;
+    } else {
+      this.leftArrowDisabled = false;
+    }
+    if (this.projectIndex == this.projectsList.length - 1) {
+      this.rightArrowDisabled = true;
+    } else {
+      this.rightArrowDisabled = false;
+    }
+  }
 }
